2ca87539640b81a99341b320e7f585109ab25b6c
kkllffaa <franciszek.zubel@wp.pl>
2021-09-16 19:53:50 +0200

    aasdasd

--- src/main/java/com/kkllffaa/meteor_utils/test/TitleScreenShaderRenderer.java
+++ src/main/java/com/kkllffaa/meteor_utils/test/TitleScreenShaderRenderer.java
@@ -1,56 +1,30 @@
 package com.kkllffaa.meteor_utils.test;
 
 import com.kkllffaa.meteor_utils.mixins.RotatingCubeMapRendererAccessor;
-import com.kkllffaa.meteor_utils.utils.MyShader;
+import meteordevelopment.meteorclient.renderer.Renderer2D;
+import meteordevelopment.meteorclient.utils.Utils;
+import meteordevelopment.meteorclient.utils.render.color.Color;
 import net.minecraft.client.gui.CubeMapRenderer;
 import net.minecraft.client.gui.RotatingCubeMapRenderer;
-
-import java.io.File;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
+import net.minecraft.util.math.MathHelper;
 
 import static org.lwjgl.opengl.GL32C.*;
 
 public class TitleScreenShaderRenderer extends RotatingCubeMapRenderer {
 	public TitleScreenShaderRenderer(CubeMapRenderer cubeMap) {
 		super(cubeMap);
-		
-		shader = new MyShader(new File("C:/Users/Franek/Desktop/l/shader.vert"), new File("C:/Users/Franek/Desktop/l/shader.frag"));
-		
-		
-		vao = glGenVertexArrays();
-		int verts = glGenBuffers();
-		
-		glBindVertexArray(vao);
-		glBindBuffer( GL_ARRAY_BUFFER, verts);
-		glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
-		
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, false, 5*4, 0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 3, GL_FLOAT, false, 5*4, 2*4);
-		
-		
 	}
 	
-	private final MyShader shader;
 	
 	private RotatingCubeMapRendererAccessor accessor() {
 		return (RotatingCubeMapRendererAccessor) this;
 	}
 	
 	
-	float[] vertices =
-			{
-					//pos	//color
-					-0.5f,  0.5f, 1f, 0f, 0f, // top left
-					 0.5f,  0.5f, 0f, 1f, 0f, // top right
-					-0.5f, -0.5f, 0f, 0f, 1f, // bottom left
-					
-					 0.5f,  0.5f, 0f, 1f, 0f, // top right
-					 0.5f, -0.5f, 0f, 1f, 1f, // bottom right
-					-0.5f, -0.5f, 0f, 0f, 1f, // bottom left
-			};
+	public static Identifier TEXTURE_TRANSPARENT = new Identifier("meteor-utils", "a.png");
 	
-	int vao;
 	
 	@Override
 	public void render(float delta, float alpha) {
@@ -60,10 +34,31 @@ public class TitleScreenShaderRenderer extends RotatingCubeMapRenderer {
 		glClearColor(0, 0.4f, 0.8f, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		shader.use(() -> {
-			glBindVertexArray(vao);
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-		});
+		Utils.mc.getTextureManager().bindTexture(TEXTURE_TRANSPARENT);
+		
+		Color color = new Color(1f, 1f, 1f, 1);
+		
+		Pair<Float, Float> h = calc(accessor().getTime()/10, Utils.getWindowHeight());
+		
+		Pair<Float, Float> w = calc(accessor().getTime()/-10, Utils.getWindowWidth());
+		
+		
+		Renderer2D.TEXTURE.begin();
+		Renderer2D.TEXTURE.texQuad(w.getRight(), h.getRight(), w.getLeft(), h.getLeft(), color);
+		Renderer2D.TEXTURE.render(null);
+		
+	}
+	
+	
+	
+	private static Pair<Float, Float> calc(float t, int w) {
+		
+		float h = (float) MathHelper.clamp(((w)*(Math.sin(t)/2+0.5)), w/2f, w);
+		float h2 = ((w/2f)-h/2)-(w/4f);
+		
+		
+		
+		return new Pair<>(h, h2);
 	}
 	
 }
