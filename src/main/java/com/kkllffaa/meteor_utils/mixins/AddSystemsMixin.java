5a53bd4c0a29b539e8b20c1e9a8d1241c69b712d
kkllffaa <franciszek.zubel@wp.pl>
2021-09-16 13:39:52 +0200

    better system initializer and filter options to stash finder

--- src/main/java/com/kkllffaa/meteor_utils/mixins/AddSystemsMixin.java
+++ src/main/java/com/kkllffaa/meteor_utils/mixins/AddSystemsMixin.java
@@ -1,26 +1,17 @@
 package com.kkllffaa.meteor_utils.mixins;
 
 import com.kkllffaa.meteor_utils.Addon;
-import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.systems.System;
 import meteordevelopment.meteorclient.systems.Systems;
 import org.apache.commons.lang3.NotImplementedException;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Mixin(value = Systems.class, remap = false)
 public abstract class AddSystemsMixin {
-	
-	@Final
-	@Shadow
-	private static final Map<Class<? extends System<?>>, System<?>> systems = new HashMap<>();
 	
 	
 	@Shadow
@@ -28,6 +19,14 @@ public abstract class AddSystemsMixin {
 		throw new NotImplementedException("mixin not working XD");
 	}
 	
+	@Inject(method = "init", at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/systems/proxies/Proxies;<init>()V", shift = At.Shift.AFTER))
+	private static void addCustomSystems(CallbackInfo ci) {
+		for (System<?> system : Addon.mySystems) {
+			add(system);
+		}
+	}
+	
+	/*
 	@Inject(method = "init", at = @At("TAIL"))
 	private static void addCustomSystems(CallbackInfo ci) {
 		
@@ -40,4 +39,5 @@ public abstract class AddSystemsMixin {
 			}
 		}
 	}
+	*/
 }
--- src/main/java/com/kkllffaa/meteor_utils/mixins/StashFinderMixin.java
+++ src/main/java/com/kkllffaa/meteor_utils/mixins/StashFinderMixin.java
@@ -1,16 +1,36 @@
 package com.kkllffaa.meteor_utils.mixins;
 
+import meteordevelopment.meteorclient.events.world.ChunkDataEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.world.StashFinder;
+import meteordevelopment.meteorclient.systems.waypoints.Waypoint;
+import meteordevelopment.meteorclient.systems.waypoints.Waypoints;
+import meteordevelopment.meteorclient.utils.world.Dimension;
+import net.minecraft.util.math.Vec3i;
+import org.apache.commons.lang3.NotImplementedException;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
 
 @Mixin(value = StashFinder.class, remap = false)
-public abstract class StashFinderMixin/* extends Module*/ {
-	/*
-	@Shadow @Final private SettingGroup sgGeneral;
+public abstract class StashFinderMixin extends Module {
 	
+	@Shadow @Final private final SettingGroup sgGeneral;
 	@Shadow public List<StashFinder.Chunk> chunks;
-	private Setting<Integer> ignorewaypointdistance;
-	private Setting<Integer> ignoreneighbourdistance;
+	
+	private Setting<Integer> ignoreWaypointDistance;
+	private Setting<Integer> ignoreNeighbourDistance;
+	private Setting<Boolean> noMessageWhenEquals;
 	
 	public StashFinderMixin(Category category, String name, String description) {
 		super(category, name, description);
@@ -19,32 +39,60 @@ public abstract class StashFinderMixin/* extends Module*/ {
 	
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void init(CallbackInfo ci) {
-		ignorewaypointdistance =  sgGeneral.add(new IntSetting.Builder()
-				.name("minimum-distance")
-				.description("The minimum distance you must be from spawn to record a certain chunk.")
+		ignoreWaypointDistance =  sgGeneral.add(new IntSetting.Builder()
+				.name("ignore-if-waypoint")
+				.description("ignore chunk if distance to nearest waypoint is below this number in blocks")
 				.defaultValue(0)
-				.min(0)
-				.sliderMax(10000)
+				.min(0).max(400)
 				.build()
 		);
-		ignoreneighbourdistance =  sgGeneral.add(new IntSetting.Builder()
-				.name("minimum-distance")
-				.description("The minimum distance you must be from spawn to record a certain chunk.")
+		ignoreNeighbourDistance =  sgGeneral.add(new IntSetting.Builder()
+				.name("ignore-if-neighbour")
+				.description("ignore chunk if distance to existing stash if below this number in chunks.")
 				.defaultValue(0)
-				.min(0)
-				.sliderMax(10000)
+				.min(0).max(10)
+				.build()
+		);
+		noMessageWhenEquals = sgGeneral.add(new BoolSetting.Builder()
+				.name("no notification when equals")
+				.description("dont send notification when chunk is alredy saved.")
+				.defaultValue(true)
 				.build()
 		);
 	}
 	
-	@Inject(method = "onChunkData", at = @At("HEAD"))
+	
+	
+	@Inject(method = "onChunkData", at = @At("HEAD"), cancellable = true)
 	private void debil(ChunkDataEvent event, CallbackInfo ci) {
-		chunks.forEach(chunk -> {
-			info(String.valueOf(chunk.chunkPos.getChebyshevDistance(event.chunk.getPos())));
-			//if (chunk.chunkPos.getChebyshevDistance(event.chunk.getPos()) > 1) {
+		
+		for (StashFinder.Chunk chunk : chunks) {
+			if (noMessageWhenEquals.get() && chunk.chunkPos.equals(event.chunk.getPos())) cancelMessage = true;
 			
-			//}
-		});
+			if (chunk.chunkPos.getChebyshevDistance(event.chunk.getPos()) < ignoreNeighbourDistance.get()) {
+				ci.cancel();
+				return;
+			}
+		}
+		
+		for (Waypoint waypoint : Waypoints.get()) {
+			if (waypoint.actualDimension == Dimension.Overworld && new Vec3i(waypoint.x, waypoint.y, 0).isWithinDistance(event.chunk.getPos().getCenterAtY(0), ignoreWaypointDistance.get())) {
+				ci.cancel();
+				return;
+			}
+		}
 	}
-	*/
+	
+	
+	
+	private boolean cancelMessage;
+	
+	@Inject(method = "onChunkData", at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/systems/modules/world/StashFinder;saveCsv()V", shift = At.Shift.AFTER), cancellable = true)
+	private void debil2(ChunkDataEvent event, CallbackInfo ci) {
+		if (cancelMessage) ci.cancel();
+		cancelMessage = false;
+	}
+	@Inject(method = "onChunkData", at = @At(value = "RETURN"))
+	private void reset(ChunkDataEvent event, CallbackInfo ci) {cancelMessage = false;}
+	
 }
